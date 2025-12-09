2d3375db23b2a03db102ab040b0bb99af1934492
56kyle <56kyleoliver@gmail.com>
2025-12-08 20:08:56 -0500

    refactor: make most functions related to prompt state checking protected and organize the module overall

--- src/atlas/prompt.py
+++ src/atlas/prompt.py
@@ -17,52 +17,6 @@ from atlas.interface import get_atlas_ui
 from atlas.interface import get_atlas_window
 
 
-def find_articles(starting_element: AXUIElementRef, maximum_depth: int = 10) -> list[AXUIElementRef]:
-    """Find all articles in the live hierarchy."""
-    return find_live(starting_element, _is_article, maximum_depth)
-
-
-def _is_article(element: AXUIElementRef) -> bool:
-    """Returns whether the provided element is an article."""
-    role: Any | None = ax_get_attribute(element, "AXSubrole")
-    return role == "AXDocumentArticle"
-
-
-def get_latest_article(starting_element: AXUIElementRef) -> AXUIElementRef:
-    """Returns the latest article in the live hierarchy."""
-    articles: list[AXUIElementRef] = find_articles(starting_element)
-    return articles[-1]
-
-
-def is_ready_for_next_prompt(starting_element: AXUIElementRef) -> bool:
-    """Returns whether the article is loading or not."""
-    latest_article: AXUIElementRef = get_latest_article(starting_element)
-    children: list[AXUIElementRef] = ax_get_children(latest_article)
-    if len(children) != 2:
-        raise ValueError(f"Article has unexpected number of children: {len(children)}")
-    heading: AXUIElementRef = children[0]
-    content: AXUIElementRef = children[1]
-
-    heading_title: Any = ax_get_attribute(heading, "AXTitle")
-    if str(heading_title) != "ChatGPT said:":
-        logger.info("Waiting for chatgpt response to begin")
-        return False
-
-    return find_copy_button(content) is not None
-
-
-def find_copy_button(starting_element: AXUIElementRef) -> AXUIElementRef | None:
-    """Returns the copy button in the live hierarchy."""
-    return find_live_first(starting_element, _is_copy_button)
-
-
-def _is_copy_button(element: AXUIElementRef) -> bool:
-    """Returns whether the passed element is the copy button."""
-    role: Any | None = ax_get_attribute(element, "AXRole")
-    description: Any | None = ax_get_attribute(element, "AXDescription")
-    return role == "AXCheckBox" and description == "Copy"
-
-
 def is_prompt_loading() -> AXUIElementRef:
     """Returns whether the latest atlas non-user prompt is still in progress of generation."""
     atlas_root: AXUIElementRef = get_atlas_ui()
@@ -74,13 +28,55 @@ def is_prompt_loading() -> AXUIElementRef:
     if atlas_main is None:
         raise ValueError("Failed to find atlas main focused tab main landmark")
     logger.info("Found main landmark")
-    present: bool = is_ready_for_next_prompt(atlas_main)
-    logger.info(present)
-    while True:
-        new_present = is_ready_for_next_prompt(atlas_main)
-        if new_present != present:
-            present: bool = new_present
-            logger.info(present)
+    return _is_ready_for_next_prompt(atlas_main)
+
+
+def _is_ready_for_next_prompt(starting_element: AXUIElementRef) -> bool:
+    """Returns whether the article is loading or not.
+
+    Expects the starting element to be the main landmark present when not in a sidebar / etc.
+    """
+    latest_article: AXUIElementRef = _get_latest_article(starting_element)
+    children: list[AXUIElementRef] = ax_get_children(latest_article)
+    if len(children) != 2:
+        raise ValueError(f"Article has unexpected number of children: {len(children)}")
+    heading: AXUIElementRef = children[0]
+    content: AXUIElementRef = children[1]
+
+    heading_title: Any = ax_get_attribute(heading, "AXTitle")
+    if str(heading_title) != "ChatGPT said:":
+        logger.info("Waiting for chatgpt response to begin")
+        return False
+
+    return _find_copy_button(content) is not None
+
+
+def find_articles(starting_element: AXUIElementRef, maximum_depth: int = 10) -> list[AXUIElementRef]:
+    """Find all articles in the live hierarchy."""
+    return find_live(starting_element, _is_article, maximum_depth)
+
+
+def _is_article(element: AXUIElementRef) -> bool:
+    """Returns whether the provided element is an article."""
+    role: Any | None = ax_get_attribute(element, "AXSubrole")
+    return role == "AXDocumentArticle"
+
+def _get_latest_article(starting_element: AXUIElementRef) -> AXUIElementRef:
+    """Returns the latest article in the live hierarchy."""
+    articles: list[AXUIElementRef] = find_articles(starting_element)
+    return articles[-1]
+
+
+def _find_copy_button(starting_element: AXUIElementRef) -> AXUIElementRef | None:
+    """Returns the copy button in the live hierarchy."""
+    return find_live_first(starting_element, __is_copy_button)
+
+
+def __is_copy_button(element: AXUIElementRef) -> bool:
+    """Returns whether the passed element is the copy button."""
+    role: Any | None = ax_get_attribute(element, "AXRole")
+    description: Any | None = ax_get_attribute(element, "AXDescription")
+    return role == "AXCheckBox" and description == "Copy"
 
 
 def get_text(element: AXUIElementRef) -> str:
@@ -103,4 +99,3 @@ def _iter_text(element: AXUIElementRef) -> Generator[str, None, None]:
 
 if __name__ == "__main__":
     is_prompt_loading()
-    # print(get_text(get_atlas_ui()))
